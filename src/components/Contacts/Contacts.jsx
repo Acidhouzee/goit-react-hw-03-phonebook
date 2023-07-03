63b0bcdc29ee58aac5ca6adcbc4ab0bc2ae751e4
Acidhouzee <Bohdankireev@gmail.com>
2023-07-03 12:26:42 +0300

    fix after review

--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -8,7 +8,7 @@ export const Contacts = ( { contacts, filter, deleteContact } ) => (
             {contacts
             ?.filter(contact => contact.name.toLowerCase().includes(filter.toLowerCase()))
             .map((contact, index) => (
-                <li className={css.feedback_options_li} key={index} id={contact.id}>
+                <li className={css.feedback_options_li} key={contact.id}>
                     {contact.name}: {contact.number}
                     <button className={css.feedback_options_li_button} type="button" onClick={() => deleteContact(contact.id)}>Delete</button>
                 </li>
